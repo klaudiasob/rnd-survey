7c4b92b66db1a3a4191521f17128876d4e6c936f
Klaudia <klau.sobotka@gmail.com>
2020-12-28 10:58:47 +0100

    Add CreativeTim views.

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,23 @@
 
 Rails.application.routes.draw do
   devise_for :admins
+
+  devise_scope :admin do
+    authenticated :admin do
+      root 'surveys#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
+
   resources :users
   resources :survey_users, only: :index
   resources :surveys do
     resources :users, except: :index, controller: 'survey_users'
     get 'answers', to: 'survey_users#index_survey'
   end
+
+
 end
