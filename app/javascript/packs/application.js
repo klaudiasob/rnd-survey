262270870e25502b6e6813a0f1190330aef57d7c
klaudiasob <61837479+klaudiasob@users.noreply.github.com>
2020-12-29 19:51:08 +0100

    Clean up javascript and scss files (#7)
    * Remove unused assets
    
    * Move javascript and scss files to javascript folder.
    
    * Update webpacker gem and add removed libs to package.json.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,10 +7,13 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("@nathanvda/cocoon")
 
-window.jQuery = $;
-window.$ = $;
+import $ from "jquery";
+global.$ = $;
+global.jQuery = $;
+
+require("@nathanvda/cocoon")
+import("../src/dashboard")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,11 +1,12 @@
-const { environment } = require('@rails/webpacker')
+const { environment } = require('@rails/webpacker');
+const webpack = require('webpack');
 
-module.exports = environment
+environment.plugins.append("Provide", new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: ['popper.js', 'default']
+}));
 
-const webpack = require('webpack')
-environment.plugins.prepend('Provide',
-    new webpack.ProvidePlugin({
-        $: 'jquery/src/jquery',
-        jQuery: 'jquery/src/jquery'
-    })
-)
+environment.config.set('resolve.alias', {jquery: 'jquery/src/jquery'});
+
+module.exports = environment;
